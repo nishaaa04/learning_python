9cbf9f4bbe0d3bb02b2a06ef8c790bd80cb20c9c
nishaaa04 <04nisham2005@gmail.com>
2026-02-21 10:15:36 +0530

    Add files via upload

---
+++ day1_EscapeSeq.py
@@ -0,0 +1,13 @@
+print("What is your name? My name is Nisha M")
+print("What is your name? \nMy name is Nisha M")
+
+#Tab Space
+print("Days\t\tMon\t\tTue\t\tWed\t\tThu\t\tFri\t\tsat\t\tSun")
+
+#double back slash
+print('Hi Nethanya! She\'s my sister')
+#print("Winston Churchill said,\"Success is not final, failure is not fatal,It is the courage to continue that counts.\"")
+print("This is a backslash:\\")
+
+print('\'It\'s not the years in your life that count. It\'s the life in your years.\'\n\t\t\t\t\t\t\t-Abraham Lincoln')
+print("\'It\'s Hero Time!\n'\t\t-Ben10")
---
+++ day1_hlo_world.py
@@ -0,0 +1,10 @@
+'''End argument'''
+print("800+256",end="=")
+print(800+256)
+
+print("Nisha","Nevilly","Satchel")
+print("Nisha","Nevilly","Satchel",sep=";")
+
+print("Wow","Wow",sep='!',end='!')
+print("Wow")
+
