44452090f568c17a65a003ac4850fe47bc879514
nishaaa04 <04nisham2005@gmail.com>
2026-02-21 22:01:09 +0530

    Add files via upload

---
+++ day2_datatypes.py
@@ -0,0 +1,31 @@
+state = "Karnataka"
+print(state)
+
+roll_no = 26
+print(roll_no)
+
+percentage = 93.333
+print(percentage)
+
+right_to_vote = True
+print(right_to_vote)
+
+#Check datatype
+print(type(state))
+print(type(roll_no))
+print(type(percentage))
+print(type(right_to_vote))
+
+#bank acc quiz
+name, balance = "Nisha",4000
+#to find the datatype of 4000
+print(type(4000))
+
+#Dynamic typing
+balance = 4000.50
+print(type(balance))
+
+#indexing
+name = "Hemoglobin"
+print(name[3])
+print(name[7])
