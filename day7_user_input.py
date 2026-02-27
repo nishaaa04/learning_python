d8b8bcb2999b200a7c5e2f9a9eb70dadae31fcf7
nishaaa04 <04nisham2005@gmail.com>
2026-02-27 20:59:22 +0530

    Add files via upload

---
+++ day7_user_input.py
@@ -0,0 +1,25 @@
+'''num = float(input("Enter a number:"))
+print(type(num))
+
+num = float(input("Enter a number:"))
+print(num + 100)
+
+num = input("Enter the value of num:")
+print(num*5)
+
+# Average of marks
+sub1 = float(input("Enter Physics marks:"))
+sub2 = float(input("Enter Maths marks:"))
+sub3 = float(input("Enter English marks:"))
+total_marks = sub1+sub2+sub3
+avg_marks = total_marks/3
+print(total_marks)
+print(avg_marks)'''
+
+#Evaluate the Experessions: 2 cube+2bc+c2
+# Evaluate: 3a2+b-2c
+a = float(input("Enter the value of a:"))
+b = float(input("Enter the value of b:"))
+c = float(input("Enter the value of c:"))
+print((2**3) + (2*b*c) +(c**2))
+print(3*(a**2) + b - (2*c))
---
+++ day_7UnitDigit.py
@@ -0,0 +1,13 @@
+#Unit Digit
+'''number =(input("Enter  a number:"))
+print(number[len(number)-1])
+#Negative Indexing
+print(number[-1])
+
+num = int(input("Enter a number:"))
+print(num%10)'''
+
+#Tenth Digit
+num = int(input("Enter a number:"))
+num = num//10
+print(num%10)
