9b372508bd48c72bcc7d1204ec6ac777bc4f4f87
nishaaa04 <04nisham2005@gmail.com>
2026-02-28 22:42:10 +0530

    Add files via upload

---
+++ day8_Statements.py
@@ -0,0 +1,33 @@
+#Conditional Statements
+#if condition statements
+if (5 == 4):
+    print('5 is equal to 5')
+    print('Program successful!')
+print("Exit")
+
+if (5 == 5):
+    print('5 is equal to 5')
+    print('Program successful!')
+print("Exit")
+
+#if and else condition statements
+#Right to vote
+age = int(input("Enter your age:"))
+if(age >= 18):
+    print("Eligible to vote." )
+else:
+    print("Not eligible to vote.")
+print("Exit...")
+
+#if,else and elif conditions
+marks = float(input("Enter your marks:"))
+if(marks<=35):
+    print("Fail")
+elif(marks<=59):
+    print("Second Class")
+elif(marks<=84):
+    print("First Class")
+else:
+    print("Distinction!")
+print("Thank You!")
+
