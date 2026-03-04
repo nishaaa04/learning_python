8e9fb2dacc343e5a870bd9b94f16e0b090c78693
nishaaa04 <04nisham2005@gmail.com>
2026-03-04 20:18:30 +0530

    Add files via upload

---
+++ day9_Nested_if.py
@@ -0,0 +1,49 @@
+#Nested if statement
+'''age = int(input("Enter your age:"))
+eats_pizza = True
+exercise = False
+if(age < 30):
+    if(eats_pizza ):
+        print("Unfit")
+    else:
+        print("Fit")
+if(age > 30):
+    if(exercise):
+        print("Unfit")
+    else:
+        print("Fit")
+
+#converting the above program into ternary operator program
+age = int(input("Enter your age:"))
+eats_pizza = True
+exercises = False
+print("Unfit"if (eats_pizza) else "Fit") if(age < 30)else("Unfit"if (exercises) else "Fit")
+
+#Ternary Operator
+#Normal if-else statement
+num = int(input("Enter a number:"))
+if (num % 2 == 0):
+    print(num,"is an even number")
+else:
+    print(num,"is a odd number")
+#Now using ternary operator,
+num = int(input("Enter a number:"))
+print("Even"if num % 2==0 else "odd")
+
+#Example2
+age = int(input("Enter your age:"))
+print("Child"if (age < 18) else"Adult")'''
+
+#Quiz
+age = int(input("Enter your age:"))
+income = int(input("Enter your income:"))
+if (age < 18):
+    message = "You are too young to work."
+elif(age >= 18) and (age <= 25):
+    if (income < 30000):
+        message = "You are eligible for a student discount."
+    else:
+        message = "You are eligible for a regular discount."
+else:
+    message = "You are eligible for a regular discount."
+print(message)
