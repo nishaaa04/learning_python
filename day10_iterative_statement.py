f230b6b91920b39ed45b78084f7331c6923870bd
nishaaa04 <04nisham2005@gmail.com>
2026-03-05 21:54:19 +0530

    Add files via upload

---
+++ day10_iterative_statement.py
@@ -0,0 +1,23 @@
+#FOR LOOP
+for i in range(100):
+    print(i)
+
+for i in range(10,100):
+    print(i)
+
+for i in range(10,100,2):
+    print(i)
+
+for i in range(150,20,-2):
+    print(i)
+
+#while loop
+num = 5
+while(num>0):
+    print("Nisha")
+    num-=1
+
+count = 0
+while count < 10:
+    print(count)
+    count += 1
